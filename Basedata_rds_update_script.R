39565449449cb74aeba9b435276a1baa6dbe2157
MattCallahan-NOAA <matt.callahan@noaa.gov>
2024-12-17 10:34:12 -0900

    changed pipe

--- Basedata_rds_update_script.R
+++ Basedata_rds_update_script.R
@@ -5,11 +5,11 @@ require(lubridate)
 
 
 #  Define the Bering Sea dataset
-base_update <- httr::content(httr::GET('https://apex.psmfc.org/akfin/data_marts/akmp/ecosystem_sub_crw_avg_sst?start_date=19850101&end_date=20501130'), type = "application/json") %>% 
-  bind_rows %>% 
-  mutate(date=as_date(READ_DATE)) %>% 
-  data.frame %>% 
-  dplyr::select(date,meansst=MEANSST,Ecosystem_sub=ECOSYSTEM_SUB)%>%
+base_update <- httr::content(httr::GET('https://apex.psmfc.org/akfin/data_marts/akmp/ecosystem_sub_crw_avg_sst?start_date=19850101&end_date=20501130'), type = "application/json") |>
+  bind_rows() |>
+  mutate(date=as_date(READ_DATE)) |>
+  data.frame() |>
+  dplyr::select(date,meansst=MEANSST,Ecosystem_sub=ECOSYSTEM_SUB) |>
   saveRDS("Data/base.rds")
 
 
